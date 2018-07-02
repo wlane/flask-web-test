5e94dc17c4990523e961baf33e40370164d8cd11
alex <xyf2424094@gmail.com>
2018-07-02 15:13:26 +0800

    add template

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-manager = Manager(app)
+#manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -14,8 +16,17 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 #    return '<h1>hello,%s!</h1>' % name
-    return render_template('user.html', name=name)
+    return render_template('user3.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__=='__main__':
 #    app.run(debug=True)
-    manager.run()
+#    manager.run()
+    app.run(host='0.0.0.0',debug=True)
